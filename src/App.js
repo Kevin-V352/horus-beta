f7f7eeaaad6104dc0f3659e87655519859943dcf
Kevin-V352 <67164849+Kevin-V352@users.noreply.github.com>
2020-12-07 22:27:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css';
+
+//----Components----//
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <p>aaaaaa</p>
+    );
+};
+
+export default Header;
